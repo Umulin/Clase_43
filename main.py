c6e12befa16d48fbd8adbf2bd27465f62347d649
unknown <santiagoviera1999@gmail.com>
2022-01-05 19:47:10 -0300

    prueba pull

--- main.py
+++ main.py
@@ -5,5 +5,14 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+'''
+    crazy = 8
+    import pdb
+    pdb.set_trace()  # is used to stop the code in a specific place and ask values of the variables and functions
+    #                              https://docs.python.org/3/library/pdb.html#pdbcommand-next
+    low = 10
+    coco = crazy + low 
+    '''
+
 if __name__ == '__main__':
     print_hi('PyCharm')
